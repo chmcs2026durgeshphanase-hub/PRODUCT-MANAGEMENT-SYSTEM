eeebbd43f85b520cc3fd7c806bbafd0dee4163e5
chmcs2026durgeshphanase-hub <chmcs2026.durgesh.phanase@gmail.com>
2026-03-08 00:55:35 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://product-management-system-backend-mpqf.onrender.com'
 })
 
 export default api
